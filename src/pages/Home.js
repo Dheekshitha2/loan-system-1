4d6dd3327b910cd349d4e729f06039131d49fa36
Dheekshitha2 <shreedhee0212@gmail.com>
2024-01-29 15:02:14 +0800

    fixed some formatting issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
 import InventoryList from '../components/InventoryList';
 
 function Home({ cart, setCart }) {
   const [selectedCategories, setSelectedCategories] = useState([]);
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+
 
   const handleCategoryChange = (category) => {
     if (Array.isArray(category) && category.length === 0) {
@@ -14,6 +17,19 @@ function Home({ cart, setCart }) {
       setSelectedCategories([...selectedCategories, category]);
     }
   };
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 200) { // Adjust as needed
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -32,7 +48,7 @@ function Home({ cart, setCart }) {
         cart={cart}
         setCart={setCart}
         selectedCategories={selectedCategories} />
-      <button className="scrollToTop-btn" onClick={scrollToTop}>Back to Top</button>
+      {showTopBtn && <button className="scrollToTop-btn" onClick={scrollToTop}>Top</button>}
     </div>
   );
 }
